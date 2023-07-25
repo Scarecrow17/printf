e0e9e704a0c7bd698742ee9db139eb2311e001f6
reyzmerly <reyzmerly9@gmail.com>
2023-07-24 23:46:01 -0700

    main.h

--- main.h
+++ main.h
@@ -23,7 +23,7 @@
  *   *
  *    * @fmt: format
  *     * @fn: associated function
- *      */
+*      **/
 struct fmt
 {
 		char fmt;
@@ -35,7 +35,7 @@ struct fmt
  *  * typedef struct fmt fmt_t - Struct op
  *    * @fmt: format
  *     * @fm_t: associated function
- *      */
+*      **/
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
@@ -114,4 +114,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
-
